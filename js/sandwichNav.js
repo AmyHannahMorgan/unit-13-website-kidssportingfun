172ab03a0dc40403e6b1877a014443675db7874f
AmyHannahMorgan <47041871+AmyHannahMorgan@users.noreply.github.com>
2019-05-28 17:19:30 +0100

    began creating framework for sandwich menu items

--- js/sandwichNav.js
+++ js/sandwichNav.js
@@ -1,6 +1,8 @@
 const sandwichOpen = document.querySelector('#sandwichOpen');
 const sandwichClose = document.querySelector('#sandwichClose');
 const sandwichNav = document.querySelector('sandwichNav');
+const sandwichBars = sandwichNav.querySelectorAll('.sandwichBar')
+const sandwichBarHandlers = [];
 
 sandwichOpen.addEventListener('click', () => toggleSandwichNav(true));
 sandwichClose.addEventListener('click', () => toggleSandwichNav(false));
@@ -12,4 +14,11 @@ function toggleSandwichNav(open){
   else {
     sandwichNav.classList.remove('open');
   }
+}
+
+function SandwichBarHandler(sandwichBar) {
+  this.sandwichBar = sandwichBar;
+  this.button = this.sandwichBar.querySelector('.sandwichBarButton');
+  this.arrow = this.button.querySelector('.sandwichBarArrow');
+  this.content = this.sandwichBar.querySelector('.sandwichBarContent');
 }
