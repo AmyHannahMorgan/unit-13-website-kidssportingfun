a3cad6429a023f26d37633589d4505a17c7cf4d2
AmyHannahMorgan <47041871+AmyHannahMorgan@users.noreply.github.com>
2019-06-01 11:16:34 +0100

    wrote code to handle sandwich nav dropdown

--- js/sandwichNav.js
+++ js/sandwichNav.js
@@ -7,6 +7,10 @@ const sandwichBarHandlers = [];
 sandwichOpen.addEventListener('click', () => toggleSandwichNav(true));
 sandwichClose.addEventListener('click', () => toggleSandwichNav(false));
 
+for (let i = 0; i < sandwichBars.length; i++) {
+  sandwichBarHandlers.push(new SandwichBarHandler(sandwichBars[i]));
+}
+
 function toggleSandwichNav(open){
   if (open) {
     sandwichNav.classList.add('open');
@@ -21,4 +25,22 @@ function SandwichBarHandler(sandwichBar) {
   this.button = this.sandwichBar.querySelector('.sandwichBarButton');
   this.arrow = this.button.querySelector('.sandwichBarArrow');
   this.content = this.sandwichBar.querySelector('.sandwichBarContent');
+
+  this.button.addEventListener('click', () => {
+    this.toggleOpen();
+  });
+
+  this.toggleOpen = () => {
+    if (this.content !== null) {
+      let currHeight = this.content.style.height;
+      if (currHeight === '') {
+        this.content.style.height = this.content.scrollHeight + 'px';
+        this.arrow.classList.add('rotate');
+      }
+      else {
+        this.content.style.height = '';
+        this.arrow.classList.remove('rotate');
+      }
+    }
+  };
 }
